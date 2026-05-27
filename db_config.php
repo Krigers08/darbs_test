e3170e8f420a7f99421c11b73b99c88d4eeec55f
Krigers08 <kristersgredzens@gmail.com>
2026-05-27 19:57:02 +0300

    Update default database password in db_config.ph
    Changed default database password to a new value.

--- db_config.php
+++ db_config.php
@@ -11,7 +11,7 @@ if ($database_url) {
     $db_port = getenv('PGPORT') ?: '5432';
     $db_name = getenv('POSTGRES_DB') ?: 'postgres';
     $db_user = getenv('PGUSER') ?: 'postgres';
-    $db_password = getenv('PGPASSWORD') ?: 'postgres';
+    $db_password = getenv('PGPASSWORD') ?: 'klSVuOHVaYdHeIUKvFdbxJpRjLBUVIwT';
 
     $dsn = "pgsql:host=$db_host;port=$db_port;dbname=$db_name;user=$db_user;password=$db_password";
 }
